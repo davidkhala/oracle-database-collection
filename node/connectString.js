d5499038e06729549fa99d9409e64d3acf1b42b1
David Liu <david.yx.liu@oracle.com>
2022-09-30 16:39:47 +0800

    [TODO] connectionString to json

--- node/connectString.js
+++ node/connectString.js
@@ -1,50 +1,73 @@
 export default class ConnectStringBuilder {
-	/**
-	 *
-	 * @param {string} DBUniqueName
-	 * @param {string} hostDomainName
-	 */
-	constructor(DBUniqueName, hostDomainName) {
-		this.setDatabaseUniqueName(DBUniqueName)
-		this.setHostDomainName(hostDomainName)
-	}
+    /**
+     *
+     * @param {string} DBUniqueName
+     * @param {string} hostDomainName
+     */
+    constructor(DBUniqueName, hostDomainName) {
+        this.setDatabaseUniqueName(DBUniqueName)
+        this.setHostDomainName(hostDomainName)
+    }
 
-	/**
-	 *
-	 * @param {string} DBUniqueName - Or PDB name, Or application service name
-	 * @returns {ConnectStringBuilder}
-	 */
-	setDatabaseUniqueName(DBUniqueName) {
-		this.DBUniqueName = DBUniqueName
-		return this
-	}
+    /**
+     *
+     * @param {string} DBUniqueName - Or PDB name, Or application service name
+     * @returns {ConnectStringBuilder}
+     */
+    setDatabaseUniqueName(DBUniqueName) {
+        this.DBUniqueName = DBUniqueName
+        return this
+    }
 
-	get serviceName() {
-		return `${this.DBUniqueName}.${this.hostDomainName}`
-	}
+    get serviceName() {
+        return `${this.DBUniqueName}.${this.hostDomainName}`
+    }
 
-	setHostnamePrefix(hostPrefix) {
-		this.hostPrefix = hostPrefix
-		return this
-	}
+    setHostnamePrefix(hostPrefix) {
+        this.hostPrefix = hostPrefix
+        return this
+    }
 
-	setHostDomainName(hostDomainName) {
-		this.hostDomainName = hostDomainName
-		return this
-	}
+    setHostDomainName(hostDomainName) {
+        this.hostDomainName = hostDomainName
+        return this
+    }
 
-	setPublicIP(ip) {
-		this.ip = ip
-		return this
-	}
+    setPublicIP(ip) {
+        this.ip = ip
+        return this
+    }
 
-	build() {
-		let FQDN
-		if (this.ip) {
-			FQDN = this.ip
-		} else {
-			FQDN = `${this.hostPrefix}.${this.hostDomainName}`;
-		}
-		return `${FQDN}:${this.port || 1521}/${this.serviceName}`
-	}
+    build() {
+        let FQDN
+        if (this.ip) {
+            FQDN = this.ip
+        } else {
+            FQDN = `${this.hostPrefix}.${this.hostDomainName}`;
+        }
+        return `${FQDN}:${this.port || 1521}/${this.serviceName}`
+    }
+}
+
+export class ConnectStringParser {
+
+
+    // TODO
+    static unitParse(str) {
+        const strTrimmed = str.trim()
+        const noBrackets = strTrimmed.replace(/^\(|\)$/g, '');
+
+        console.debug(noBrackets)
+        const tokens = noBrackets.match(/.*(?<==)/)
+
+        console.debug({tokens})
+
+        // const [key, value] = noBrackets.split('=')
+        // console.debug({key,value})
+        //
+        // return {
+        //     [key.trim()]: ConnectStringParser.unitParse(value)
+        // }
+    }
+
 }
--- node/test/connectTest.js
+++ node/test/connectTest.js
@@ -1,18 +1,33 @@
 import ConnectionManager from '../connection.js'
-import Builder from '../connectString.js'
+import Builder, {ConnectStringParser} from '../connectString.js'
 import DBConfig from '../dbConfig.js'
-describe('non-pool connection', () => {
-	const {password, DBUniqueName, hostDomainName, ip} = process.env;
-	it('touch', async () => {
-		const config = new DBConfig({
-			user: "sys",
-			password,
-			connectString: new Builder(DBUniqueName, hostDomainName).setPublicIP(ip).build()
-		})
-		const connectionBuilder = new ConnectionManager(config)
-		await connectionBuilder.connect()
-		await connectionBuilder.close()
-	})
 
+describe('non-pool connection', () => {
+    const {password, DBUniqueName, hostDomainName, ip} = process.env;
+    it('touch', async () => {
+        const config = new DBConfig({
+            user: "sys",
+            password,
+            connectString: new Builder(DBUniqueName, hostDomainName).setPublicIP(ip).build()
+        })
+        const connectionBuilder = new ConnectionManager(config)
+        await connectionBuilder.connect()
+        await connectionBuilder.close()
+    })
+
+    it('connect string parser', () => {
+        const str = ` 	(description=
+            (retry_count=20)
+            (retry_delay=3)
+            (address=(protocol=tcps)(port=1522)(host=adb.ap-singapore-1.oraclecloud.com))
+        (connect_data=(service_name=g2deddcd02b6db5_adw_high.adb.oraclecloud.com))
+        (security=(ssl_server_cert_dn="CN=adb.ap-singapore-1.oraclecloud.com, OU=Oracle ADB SINGAPORE, O=Oracle Corporation, L=Redwood City, ST=California, C=US"))
+    )`
+        const str1 = '(abc=1)'
+        const str2 = `(description=(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-singapore-1.oraclecloud.com))(connect_data=(service_name=g2deddcd02b6db5_adw_high.adb.oraclecloud.com))(security=(ssl_server_cert_dn="CN=adb.ap-singapore-1.oraclecloud.com, OU=Oracle ADB SINGAPORE, O=Oracle Corporation, L=Redwood City, ST=California, C=US")))`
+
+        const result = ConnectStringParser.parse(str2)
+        console.debug({result})
+    })
 
 })
