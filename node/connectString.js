9867ce702908d1395f07e00751fdbb666c1540f8
David Liu <david.yx.liu@oracle.com>
2022-10-13 21:27:45 +0800

    [DONE] connectionString to json

--- node/connectString.js
+++ node/connectString.js
@@ -1,3 +1,5 @@
+import assert from 'assert'
+
 export default class ConnectStringBuilder {
     /**
      *
@@ -12,10 +14,12 @@ export default class ConnectStringBuilder {
     set databaseUniqueName(DBUniqueName) {
         this.DBUniqueName = DBUniqueName
     }
-    set pdbName(name){
+
+    set pdbName(name) {
         this.DBUniqueName = name
     }
-    set applicationServiceName(name){
+
+    set applicationServiceName(name) {
         this.DBUniqueName = name
     }
 
@@ -38,7 +42,7 @@ export default class ConnectStringBuilder {
         return this
     }
 
-    get FQDN(){
+    get FQDN() {
 
         if (this.ip) {
             return this.ip
@@ -46,11 +50,13 @@ export default class ConnectStringBuilder {
             return `${this.hostPrefix}.${this.hostDomainName}`;
         }
     }
+
     build() {
 
         return `${this.FQDN}:${this.port || 1521}/${this.serviceName}`
     }
-    buildForAnalyticServer(){
+
+    buildForAnalyticServer() {
         return `${this.FQDN}:${this.port || 1521}:${this.serviceName}`
     }
 }
@@ -58,22 +64,96 @@ export default class ConnectStringBuilder {
 export class ConnectStringParser {
 
 
-    // TODO
-    static unitParse(str) {
-        const strTrimmed = str.trim()
-        const noBrackets = strTrimmed.replace(/^\(|\)$/g, '');
+    static parse(str) {
 
-        console.debug(noBrackets)
-        const tokens = noBrackets.match(/.*(?<==)/)
+        const findEndBracket = (str) => {
+            let level = 0
+            let escapeStatus = false
+            for (let i = 0; i < str.length; i++) {
+                const char = str[i]
+                switch (char) {
+                    case '(':
+                        level++
+                        break;
+                    case ')':
+                        level--
+                        break;
+                    case '"':
+                        escapeStatus = !escapeStatus
+                        break;
+                }
+                if (level === 0) {
+                    return i
+                }
+            }
+            return -1
+        }
 
-        console.debug({tokens})
+        const trim = str.replace(/\s/g, '')
 
-        // const [key, value] = noBrackets.split('=')
-        // console.debug({key,value})
-        //
-        // return {
-        //     [key.trim()]: ConnectStringParser.unitParse(value)
-        // }
+        let escapeStatus = false
+        let keyStart = 0, keyEnd = 0, valueStart = 0, valueEnd = 0
+        let key
+        let expectValue = false, isArrayValue = false
+        const result = {}
+        for (let charIndex = 0; charIndex < trim.length; charIndex++) {
+            const char = trim[charIndex]
+            if (char.match(/\w/)) {
+                continue
+            }
+            if (char === '"') {
+                escapeStatus = !escapeStatus
+                if (escapeStatus && expectValue) {
+                    valueStart = charIndex + 1
+                }
+                if (!escapeStatus && expectValue) {
+                    valueEnd = charIndex
+                }
+
+
+                continue
+            }
+            if (escapeStatus) {
+                continue
+            }
+            switch (char) {
+                case '(':
+                    if (expectValue) {
+                        const bracketIndex = findEndBracket(trim.slice(charIndex))
+                        assert.ok(bracketIndex > 0, 'Invalid format: End bracket not found')
+                        if (!result[key]) {
+                            result[key] = {}
+                        }
+                        for (const [_key, _value] of Object.entries(this.parse(trim.slice(charIndex, charIndex + bracketIndex + 1)))) {
+                            result[key][_key] = _value
+                        }
+
+                        charIndex += bracketIndex
+                        isArrayValue = true
+                    } else {
+                        keyStart = charIndex + 1
+                    }
+
+
+                    break;
+                case '=':
+                    key = trim.slice(keyStart, charIndex)
+                    valueStart = charIndex + 1
+                    expectValue = !expectValue
+                    break;
+                case ')':
+                    if (!isArrayValue) {
+                        expectValue = !expectValue
+                        if (valueEnd === 0) {
+                            valueEnd = charIndex
+                        }
+                        result[key] = trim.slice(valueStart, valueEnd)
+                        valueEnd = 0
+                    }
+                    break;
+            }
+        }
+        return result
     }
 
 }
--- node/test/connectTest.js
+++ node/test/connectTest.js
@@ -1,7 +1,6 @@
 import ConnectionManager from '../connection.js'
 import Builder, {ConnectStringParser} from '../connectString.js'
 import DBConfig from '../dbConfig.js'
-
 const touch = async ({password, DBUniqueName, hostDomainName, ip, connectString}) => {
     if (!connectString) {
         connectString = new Builder(DBUniqueName, hostDomainName).setPublicIP(ip).build()
@@ -47,9 +46,27 @@ describe('connection', function () {
     )`
         const str1 = '(abc=1)'
         const str2 = `(description=(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-singapore-1.oraclecloud.com))(connect_data=(service_name=g2deddcd02b6db5_adw_high.adb.oraclecloud.com))(security=(ssl_server_cert_dn="CN=adb.ap-singapore-1.oraclecloud.com, OU=Oracle ADB SINGAPORE, O=Oracle Corporation, L=Redwood City, ST=California, C=US")))`
+        const str3 = '(abc=1)(second = (retry_count=20))'
 
-        const result = ConnectStringParser.parse(str2)
-        console.debug({result})
+        console.debug(ConnectStringParser.parse(str1))
+        console.debug(ConnectStringParser.parse(str2))
+        console.debug(ConnectStringParser.parse(str3))
+    })
+    it('dev', ()=>{
+
+        const str = `
+         	(description=
+                (retry_count=20)
+                (retry_delay=3)
+                (address=
+                    (protocol=tcps)
+                    (port=1522)
+                    (host=adb.ap-singapore-1.oraclecloud.com)
+                )
+                (connect_data=(service_name=g2deddcd02b6db5_adw_high.adb.oraclecloud.com))
+                (security=(ssl_server_cert_dn="CN=adb.ap-singapore-1.oraclecloud.com, OU=Oracle ADB SINGAPORE, O=Oracle Corporation, L=Redwood City, ST=California, C=US"))
+    )`
+        console.warn(JSON.stringify(ConnectStringParser.parse(str)))
     })
 
 })
