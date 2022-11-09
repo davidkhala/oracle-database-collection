cd9d176a2a00f18a45997d31518ff8e6e5ed68b6
David Liu <david.yx.liu@oracle.com>
2022-11-09 15:49:46 +0800

    add placeholder for SQLDeveloper affinity

--- SQL/user.sql
+++ SQL/user.sql
@@ -1,9 +1,12 @@
 
 -- Create user
-CREATE USER username IDENTIFIED BY "Tr4v1&Pa$$w0rd";
+CREATE USER "&username" IDENTIFIED BY "Tr4v1an&Pa$$w0rd";
 
--- Common grant privilege
-GRANT CONNECT, RESOURCE, DBA TO username;
-GRANT create session, grant any privilege to username;
-GRANT UNLIMITED TABLESPACE TO username;
+-- Common grant privilege, ADB compatible, tested in SQL Developer
+GRANT CONNECT, RESOURCE TO &username;
+GRANT create session to &username;
+GRANT UNLIMITED TABLESPACE TO &username;
 
+-- ODB classic only privilege
+GRANT DBA TO &username;
+Grant ALL PRIVILEGES TO &username
