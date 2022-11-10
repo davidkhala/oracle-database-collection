2d1695e4d479ea9bd0a84e227a588b947f1889c7
David Liu <david.yx.liu@oracle.com>
2022-11-10 10:31:46 +0800

    [DONE] roles mapping

---
+++ SQL/role.sql
@@ -0,0 +1,8 @@
+-- Grantee, role and option
+select * from dba_role_privs;
+-- Grantee, privilege and option
+select * from dba_sys_privs;
+-- Role and privilege mapping
+select * from role_sys_privs;
+-- Inheritance hierarchy between roles
+select * from role_role_privs;
