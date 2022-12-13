acab4b1f3f77665d38548c8aa81603e32c8e9404
David Liu <david.yx.liu@oracle.com>
2022-12-13 11:31:59 +0800

    Update troubleshoot.sql

--- SQL/troubleshoot.sql
+++ SQL/troubleshoot.sql
@@ -2,6 +2,5 @@
 select *
 from dba_hist_sqltext;
 -- show recent query, here is a 60-days sample
-SELECT *
-FROM V$SQL V
-where to_date(v.FIRST_LOAD_TIME, 'YYYY-MM-DD hh24:mi:ss') > sysdate - 60
+SELECT SQL_TEXT, PARSING_SCHEMA_NAME, SERVICE, FIRST_LOAD_TIME, LAST_LOAD_TIME FROM V$SQL V
+where to_date(v.FIRST_LOAD_TIME, 'YYYY-MM-DD hh24:mi:ss') > sysdate - 60 AND PARSING_SCHEMA_NAME!='SYS'
