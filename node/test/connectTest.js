1e9fe77294f0c5ee57c8476b5ffcb97b9aa477c9
David Liu <david-khala@hotmail.com>
2024-04-15 10:35:03 +0800

    add varmeego example

--- node/test/connectTest.js
+++ node/test/connectTest.js
@@ -15,11 +15,19 @@ describe('connection', function () {
         const str1 = '(abc=1)';
         const str2 = '(description=(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-singapore-1.oraclecloud.com))(connect_data=(service_name=g2deddcd02b6db5_adw_high.adb.oraclecloud.com))(security=(ssl_server_cert_dn="CN=adb.ap-singapore-1.oraclecloud.com, OU=Oracle ADB SINGAPORE, O=Oracle Corporation, L=Redwood City, ST=California, C=US")))';
         const str3 = '(abc=1)(second = (retry_count=20))';
-
+        const varmeego = `(DESCRIPTION =
+    (ADDRESS = (PROTOCOL = TCP)(HOST = 172.17.240.112)(PORT = 1521))
+    (CONNECT_DATA =
+      (SERVER = DEDICATED)
+      (SERVICE_NAME = TSW3_DEV)
+    )
+  )
+`
         console.debug(ConnectStringParser.parse(str1));
         console.debug(ConnectStringParser.parse(str3));
         console.debug(ConnectStringParser.parse(str2));
         console.debug(ConnectStringParser.parse(str))
+        console.debug(ConnectStringParser.parse(varmeego))
     });
 
 
