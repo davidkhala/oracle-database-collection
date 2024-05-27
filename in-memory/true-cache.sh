308e16a2fd61dd4398cc61a3406587d74e3fa14e
David Liu <david-khala@hotmail.com>
2024-05-27 16:37:57 +0800

    true-cache cannot be setup

--- in-memory/true-cache.sh
+++ in-memory/true-cache.sh
@@ -1,6 +1,7 @@
 # https://github.com/oracle/docker-images/blob/main/OracleDatabase/SingleInstance/README.md#running-oracle-true-cache-in-a-container-supported-from-version-2340-onwards
+password=${password:-password}
 podman-start() {
-    echo "PassW0rd__" | podman secret create oracle_pwd -
+    echo "$password" | podman secret create oracle_pwd -
     podman network create tc_net
       podman inspect tc_net | grep -iw 'subnet'
       local PRI_DB_FREE_IP="10.89.0.0"
@@ -17,20 +18,22 @@ podman-start() {
           -e PRIMARY_DB_PWD_FILE=/var/tmp/orapwFREE \
           -e PRIMARY_DB_CONN_STR=$PRI_DB_FREE_IP:1521/FREE \
       container-registry.oracle.com/database/free
-      podman cp pri-db-free:/opt/oracle/product/23ai/dbhomeFree/dbs/orapwFREE tru-cc-free:/var/tmp/
-
-      # configure
-      podman exec -it pri-db-free bash
-      --
 
 
-  $ORACLE_HOME/bin/dbca -configureDatabase -configureTrueCacheInstanceService -sourceDB FREE \
-  -trueCacheConnectString <TRU_CC_FREE_IP>:1521/FREE -trueCacheServiceName sales_pdb_tc -serviceName FREEPDB1 \
-  -pdbName FREEPDB1 -sysPassword $(cat /run/secrets/oracle_pwd) -silent
+
+
+
+
 }
 setup(){
-  dbca -configureDatabase -configureTrueCacheInstanceService -sourceDB FREE -trueCacheConnectString primary:1521/FREE -trueCacheServiceName sales_tc -serviceName FREE -sysPassword "password" -silent
+  docker cp primary:/opt/oracle/product/23ai/dbhomeFree/dbs/orapwFREE ./orapwFREE
+  docker cp ./orapwFREE true-cache:/var/tmp/orapwFREE
+  #  podman cp primary:/opt/oracle/product/23ai/dbhomeFree/dbs/orapwFREE
+  # [FATAL] [DBT-19953] True Cache is not in redo apply mode.
+      #   CAUSE: True Cache should be in READ ONLY WITH APPLY mode
 
+  dbca -configureDatabase -configureTrueCacheInstanceService -sourceDB FREE -trueCacheConnectString primary:1521/FREE -trueCacheServiceName sales_tc -serviceName FREE -sysPassword password -silent
+  dbca -configureDatabase -configureTrueCacheInstanceService -sourceDB FREE -trueCacheConnectString true-cache:1521/FREE -trueCacheServiceName sales_pdb_tc -serviceName FREEPDB1 -pdbName FREEPDB1 -sysPassword $(cat /run/secrets/oracle_pwd) -silent
 }
 podman-inspect(){
   podman inspect pri-db-free | jq -e ".[0].State.Health"
