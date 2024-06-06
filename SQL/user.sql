a2a14e8718445b56397d328d2943f219edf67986
David Liu <david-khala@hotmail.com>
2024-06-06 18:30:47 +0800

    close #3

--- SQL/user.sql
+++ SQL/user.sql
@@ -9,8 +9,26 @@ GRANT CONNECT, RESOURCE TO ${username};
 GRANT create session to ${username};
 GRANT UNLIMITED TABLESPACE TO ${username};
 
+-- View privileges
+
+select *
+from DBA_ROLE_PRIVS; -- Roles granted to users and roles
+
+select *
+from role_role_privs; -- Roles which are granted to roles
+
+select *
+from role_sys_privs; -- System privileges granted to roles
+
+select *
+from role_tab_privs;
+-- Table privileges granted to roles
+
+
 -- listAllUsers
-SELECT * FROM dba_users;
+SELECT *
+FROM dba_users;
 
 
-select user from dual; --  get currently login user
+select user
+from dual; --  get currently login user
